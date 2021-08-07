c3c96d7005b78e1cf69709b52002ecbaee6f0b85
Daniel Davidson <r3dd3ath@gmail.com>
2021-08-06 19:24:04 -0500

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -2,5 +2,5 @@
 
 $modulename = 'Watch-Command'
 mkdir $HOME\Documents\WindowsPowerShell\Modules\$modulename -Force
-Copy-Item -Recurse -Path .\Watch-Command\Watch-Command.psm1 -Destination $HOME\Documents\WindowsPowerShell\Modules\$modulename -Force | Out-Null
+Copy-Item -Recurse -Path .\Watch-Command\Watch-Command.psm1 -Destination $HOME\Documents\WindowsPowerShell\Modules\$modulename -Force
 Import-Module Watch-Command
